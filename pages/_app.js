b69df0dd0342ced31cac4a1e8853296582ab64d4
gzheng0214 <gzheng0214@gmail.com>
2021-09-06 21:27:13 -0400

    Added toast containers

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import "../styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout {...pageProps}>
       <Component {...pageProps} />
+      <ToastContainer />
     </Layout>
   );
 }
