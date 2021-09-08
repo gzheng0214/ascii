39731a4c0958752ff988919c338b2d87d5dee83a
gzheng0214 <gzheng0214@gmail.com>
2021-09-08 02:12:05 -0400

    Added active style to navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -5,25 +5,50 @@ import Button from "@material-ui/core/Button";
 import * as styles from "./navbar.module.css";
 import Link from "next/link";
 import { logout } from "../../utils/authUser";
+import { useRouter } from "next/router";
 
 const Navbar = ({ user }) => {
+  const router = useRouter();
+
+  const isActive = (route) => {
+    return router.pathname === route;
+  };
   return (
     <AppBar position="static">
       <Toolbar className={styles.toolbar}>
         {!user ? (
           <>
-            {" "}
             <Link href="/login">
-              <Button color="inherit">Login</Button>
+              <Button
+                color="inherit"
+                className={isActive("/login") ? styles.active : ""}
+              >
+                Login
+              </Button>
             </Link>
             <Link href="/register">
-              <Button color="inherit">Register</Button>
-            </Link>{" "}
+              <Button
+                color="inherit"
+                className={isActive("/register") ? styles.active : ""}
+              >
+                Register
+              </Button>
+            </Link>
           </>
         ) : (
-          <Button color="inherit" onClick={() => logout()}>
-            Logout
-          </Button>
+          <>
+            <Link href="/">
+              <Button
+                color="inherit"
+                className={isActive("/") ? styles.active : ""}
+              >
+                Convert
+              </Button>
+            </Link>
+            <Button color="inherit" onClick={() => logout()}>
+              Logout
+            </Button>
+          </>
         )}
       </Toolbar>
     </AppBar>
